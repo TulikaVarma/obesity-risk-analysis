5128f6922330f980955c4264a69741786bd067a5
kevinss09 <ksa230@sfu.ca>
2025-10-22 11:17:53 -0700

    Read dataset

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('ObesityDataSet_raw_and_data_sinthetic.csv')
+
+print(dataset.head(3))
